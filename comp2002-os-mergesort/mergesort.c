04e60b680892c360068ad2fb550dd0d6850ddf9d
root <root@BaoAu.localdomain>
2025-10-13 10:25:32 +1030

    Added code for my_mergesort() for using as base case (single-thread) for parallel_mergesort()

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -13,6 +13,14 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 
 /* this function will be called by parallel_mergesort() as its base case. */
 void my_mergesort(int left, int right){
+	// base case 
+	if(left >= right) return;
+
+	// recursive case (divide and conquer standard mergesort)
+	int mid = (left + right) / 2;				// Find the mid point
+	my_mergesort(left, mid);					// Sort the left half
+	my_mergesort(mid + 1, right);				// Sort the right half
+	merge(left, mid, mid + 1, right);			// Merge the two halves
 }
 
 /* this function will be called by the testing program. */
